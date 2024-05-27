067eb0a2a69c8eeb0d73f7c70de475d3f82eec33
Irfan Fauzi <irfanfauziyk@gmail.com>
2024-05-27 10:32:22 +0700

    membuat baseclass

--- encapsulation/encapsulation.cpp
+++ encapsulation/encapsulation.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class remoteLampu {
 private:
     string saklarNo[10];
-    public:
+public:
     void setSaklarNo(int i, string value) {
         saklarNo[1] = value;
     }
---
+++ finalspecifier/finalspecifier.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class baseClass {
+public:
+    virtual void perkenalan() {    //final ditambahkan belakang class/void
+        cout << "Halo saya Function dari base class";
+    }
+};
+
+
+int main()
+{
+    
+}
+
+
